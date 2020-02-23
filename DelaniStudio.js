f0b9a1fc534b83ecf77b5e45e02406c731845519
monte <eogenche@yahoo.com>
2020-02-23 11:19:25 +0300

    toggle function added

---
+++ DelaniStudio.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+    $("#hidd,#design").click(function() {
+        $("#design").toggle();
+        $("#hidd").toggle();
+    });
+
+    $("#styleDiv1-hidden,#styleDiv1-show").click(function() {
+      $("#styleDiv1-hidden").toggle();
+      $("#styleDiv1-show").toggle();
+
+    });
+
+    $("#styleDiv2-hidden,#styleDiv2-show").click(function() {
+      $("#styleDiv2-hidden").toggle();
+      $("#styleDiv2-show").toggle();
+       
+    });
+
+});
+
+
+    
