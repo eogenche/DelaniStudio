ebf23d1bfc9e3d2e06ea80771e16e36dd49430d5
monte <eogenche@yahoo.com>
2020-02-24 09:59:53 +0300

    hover function added

--- DelaniStudio.js
+++ DelaniStudio.js
@@ -19,5 +19,74 @@ $(document).ready(function() {
 
 });
 
+$(".text-describe").hide();
+
+  $("#port1").hover(function(){
+    $(".p1").fadeIn();
+    $(".p1").show();
+  }, function(){
+    $(".p1").hide();
+  });
+
+  $("#port2").hover(function(){
+    $(".p2").fadeIn();
+    $(".p2").show();
+  }, function(){
+    $(".p2").hide();
+  });
+
+  $("#port3").hover(function(){
+    $(".p3").fadeIn();
+    $(".p3").show();
+  }, function(){
+    $(".p3").hide();
+  });
+
+  $("#port4").hover(function(){
+    $(".p4").fadeIn();
+    $(".p4").show();
+  }, function(){
+    $(".p4").hide();
+  });
+
+  $("#port5").hover(function(){
+    $(".p5").fadeIn();
+    $(".p5").show();
+  }, function(){
+    $(".p5").hide();
+  });
+
+  $("#port6").hover(function(){
+    $(".p6").fadeIn();
+    $(".p6").show();
+  }, function(){
+    $(".p6").hide();
+  });
+
+  $("#port7").hover(function(){
+    $(".p7").fadeIn();
+    $(".p7").show();
+  }, function(){
+    $(".p7").hide();
+  });
+
+  $("#port8").hover(function(){
+    $(".p8").fadeIn();
+    $(".p8").show();
+  }, function(){
+    $(".p8").hide();
+  });
+
+
+function clicke
+d(){
+var name=document.getElementById("name").value;
+alert("Thank you "+ name +"; We have received your message");
+event.preventDefault();
+}
+
+
+
+
 
     
